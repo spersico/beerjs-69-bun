2a13b3ba60af7f53d806b816e2083240a0cad337
SP <spersico@railz.ai>
2022-10-27 03:21:24 -0300

    simplify test

--- deno.ts
+++ deno.ts
@@ -1,7 +1,5 @@
 import { serve } from 'https://deno.land/std@0.119.0/http/server.ts';
 
-const POSTS_ROUTE = new URLPattern({ pathname: '/api/posts/:id' });
-
 function handler(_req: Request): Response {
   return new Response('Hello, World!');
 }
@@ -9,3 +7,4 @@ console.log('Listening on http://localhost:8000');
 serve(handler);
 
 // deno run --allow-net https://examples.deno.land/http-server.ts
+// sudo siege http://127.0.0.1:8000 -t10s
