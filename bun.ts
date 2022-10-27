4414367b40f0d73aa7fd9845cd13f29c644d9a77
SP <spersico@railz.ai>
2022-10-27 02:44:27 -0300

    bun

---
+++ bun.ts
@@ -0,0 +1,22 @@
+import { Hono } from 'hono';
+import { logger } from 'hono/logger';
+import { cors } from 'hono/cors';
+
+const app = new Hono();
+app.use('*', logger());
+app.use('*', cors());
+
+const api = new Hono();
+api.post('/posts', async (c) => {
+  const post = await c.req.json();
+  return c.json({ incoming: post });
+});
+
+app.route('/api', api);
+
+export default {
+  port: 3000,
+  fetch: app.fetch,
+};
+
+// bun bun.ts
