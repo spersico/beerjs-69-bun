687e33320f0afe17afd03595cec648940c68d1d8
SP <spersico@railz.ai>
2022-10-27 04:00:08 -0300

    deno-hono

--- deno.ts
+++ deno.ts
@@ -1,10 +1,11 @@
-import { serve } from 'https://deno.land/std@0.119.0/http/server.ts';
+import { serve } from 'https://deno.land/std/http/server.ts';
+import { Hono } from 'https://deno.land/x/hono/mod.ts';
 
-function handler(_req: Request): Response {
-  return new Response('Hello, World!');
-}
-console.log('Listening on http://localhost:8000');
-serve(handler);
+const app = new Hono();
+
+app.get('/', (c) => c.text('Hello! Hono!'));
+
+serve(app.fetch);
 
 // deno run --allow-net https://examples.deno.land/http-server.ts
 // sudo siege http://127.0.0.1:8000 -t10s
